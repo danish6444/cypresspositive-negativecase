23445de91e27431ada5858b77649bc078ab387c2
danish6444 <139736946+danish6444@users.noreply.github.com>
2024-01-15 23:44:14 +0530

    Add files via upload

---
+++ positive&negativacase.cy.js
@@ -0,0 +1,14 @@
+describe('my first test', () => {
+
+    it('verify title-positive', () => {
+    cy.visit("https://opensource-demo.orangehrmlive.com/")
+    cy.title().should('eq','OrangeHRM')
+
+    })
+    
+    it('verify title-positive', () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/")
+        cy.title().should('eq','OrangeHRM123')
+
+})
+})
